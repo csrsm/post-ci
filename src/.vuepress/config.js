6c66bf4977d187c16e01ff1fe454882baf0d7bfb
csrsm <245129248@qq.com>
2022-12-08 10:53:03 +0800

    背景图片

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -47,10 +47,10 @@ module.exports = {
   
         // 组织
         organization: 'DaLian University of Technology',
-  
+        avatar: '/img/cm.jpg',
         // 头像
         // 设置为外部链接
-        avatar: 'http://img.icoisini.xyz/cm.jpg',
+        // avatar: 'http://img.icoisini.xyz/cm.jpg',
         // 或者放置在 .vuepress/public 文件夹，例如 .vuepress/public/img/avatar.jpg
         // avatar: '/img/avatar.jpg',
         
@@ -71,7 +71,7 @@ module.exports = {
         background: {
           // 使用图片的 URL，如果设置了图片 URL，则不会生成随机变化的图案，下面的 useGeo 将失效
           //url: 'http://img.icoisini.xyz/header.jpg',
-          url:' http://img.icoisini.xyz/background.jpg',
+          url:'/img/background.jpg',
   
           // 使用随机变化的图案，如果设置为 false，且没有设置图片 URL，将显示为空白背景
           useGeo: true,
@@ -98,8 +98,9 @@ module.exports = {
         // 卡片 header 的背景，可以使用图片，或者随机变化的图案（geopattern）
         headerBackground: {
           // 使用图片的 URL，如果设置了图片 URL，则不会生成随机变化的图案，下面的 useGeo 将失效
-          url: 'http://img.icoisini.xyz/wx-background.jpg',
-  
+          url: '/img/wx-background.jpg',
+          // url: 'http://img.icoisini.xyz/wx-background.jpg',
+
           // 使用随机变化的图案，如果设置为 false，且没有设置图片 URL，将显示为空白背景
           useGeo: true,
         },
